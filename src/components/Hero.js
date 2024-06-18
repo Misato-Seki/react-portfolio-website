ce9e38824f7270890b752fdf02e9e65fbb278335
Misato-Seki <116722027+Misato-Seki@users.noreply.github.com>
2024-06-18 09:24:00 +0300

    Update: Skills

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,7 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { useState, useEffect } from "react";
 import heroImg from '../assets/img/hero.png';
+import 'animate.css';
 // import TrackVisibility from "react-on-screen";
 
 function Hero() {
@@ -25,10 +26,10 @@ function Hero() {
             <Container>
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
-                        <h1><span className="wrap">{text}</span></h1>
+                        <h1 className="animate__animated animate__fadeIn"><span className="wrap">{text}</span></h1>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
-                        <img src={heroImg} alt="hero-img"/>
+                        <img src={heroImg} alt="hero-img" className="animate__animated animate__zoomIn"/>
                     </Col>
                 </Row>
             </Container>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,95 +1,96 @@
 import { Container, Row, Col } from "react-bootstrap";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+import 'animate.css';
+import TrackVisibility from "react-on-screen";
 
 function Skills() {
     const responsive = {
         superLargeDesktop: {
-          // the naming can be any, depends on you.
-          breakpoint: { max: 4000, min: 3000 },
-          items: 8
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 4
         },
         desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 4
+            breakpoint: { max: 3000, min: 1024 },
+            items: 4
         },
         tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 4
+            breakpoint: { max: 1024, min: 464 },
+            items: 3
         },
         mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 4
+            breakpoint: { max: 464, min: 0 },
+            items: 2
         }
-      };
+    };
     return (
         <Container>
-            <div className="skills" id="skills">
+            <TrackVisibility>
+            {({ isVisible }) =>
+            <div className={ isVisible ? "skills animate__animated animate__fadeInUp" : "skills" } id="skills">
                 <h1>Skills</h1>
-                    <Row>
-                        <Col xs={12} md={6} xl={6} className="skills-slider">
-                        <Carousel responsive={responsive}>
-                                <h2>Front-End</h2>
-                                <li>
-                                    <h3>HTML&CSS</h3>
-                                    <span className="bar"><span className="html-css">90%</span></span>
-                                </li>
-                                <li>
-                                    <h3>JavaScript</h3>
-                                    <span className="bar"><span className="javascript">85%</span></span>
-                                </li>
-                                <li>
-                                    <h3>React</h3>
-                                    <span className="bar"><span className="react">75%</span></span>
-                                </li>
-                                <li>
-                                    <h3>Node.js</h3>
-                                    <span className="bar"><span className="node-js"></span></span>
-                                </li>
-                                <li>
-                                    <h3>Responsive Design</h3>
-                                    <span className="bar"><span className="responsive"></span></span>
-                                </li>
-                                <li>
-                                    <h3>Restful APIs</h3>
-                                    <span className="bar"><span className="api"></span></span>
-                                </li>
-                                <li>
-                                    <h3>MySQL</h3>
-                                    <span className="bar"><span className=""></span></span>
-                                </li>
-                        </Carousel>
-                        </Col>
-                        <Col xs={12} md={6} xl={6} className="skills-slider">
-                            <h2>Back-End</h2>
-                            <li>
-                                <h3>C#</h3>
-                                <span className="bar"><span className="csharp"></span></span>
-                            </li>
-                            <li>
-                                <h3>Java</h3>
-                                <span className="bar"><span className="java"></span></span>
-                            </li>
-                            <li>
-                                <h3>Python</h3>
-                                <span className="bar"><span className="python"></span></span>
-                            </li>
-                            <li>
-                                <h3>Data Structures & Algorithms</h3>
-                                <span className="bar"><span className="ds-algo"></span></span>
-                            </li>
-                            <li>
-                                <h3>Linux</h3>
-                                <span className="bar"><span className="linux"></span></span>
-                                </li>
-                            <li>
-                                <h3>Git</h3>
-                                <span className="bar"><span className="git"></span></span>
-                            </li>
-                        </Col>
-                    </Row>
-
+                <h2>Front-End</h2>
+                <Carousel responsive={responsive} className="skills-slider">
+                    <li>
+                        <h3>HTML&CSS</h3>
+                        <span className="bar"><span className="html-css">90%</span></span>
+                    </li>
+                    <li>
+                        <h3>JavaScript</h3>
+                        <span className="bar"><span className="javascript">85%</span></span>
+                    </li>
+                    <li>
+                        <h3>React</h3>
+                        <span className="bar"><span className="react" style={{ width: '75%' }}>75%</span></span>
+                    </li>
+                    <li>
+                        <h3>Node.js</h3>
+                        <span className="bar"><span className="node-js" style={{ width: '70%' }}>70%</span></span>
+                    </li>
+                    <li>
+                        <h3>Responsive Design</h3>
+                        <span className="bar"><span className="responsive" style={{ width: '80%' }}>80%</span></span>
+                    </li>
+                    <li>
+                        <h3>Restful APIs</h3>
+                        <span className="bar"><span className="api" style={{ width: '70%' }}>70%</span></span>
+                    </li>
+                    <li>
+                        <h3>MySQL</h3>
+                        <span className="bar"><span className="sql" style={{ width: '50%' }}>50%</span></span>
+                    </li>
+                </Carousel>
+                <h2>Back-End</h2>
+                <Carousel responsive={responsive} className="skills-slider">
+                    <li>
+                        <h3>C#</h3>
+                        <span className="bar"><span className="csharp" style={{ width: '50%' }}>50%</span></span>
+                    </li>
+                    <li>
+                        <h3>Java</h3>
+                        <span className="bar"><span className="java" style={{ width: '50%' }}>50%</span></span>
+                    </li>
+                    <li>
+                        <h3>Python</h3>
+                        <span className="bar"><span className="python" style={{ width: '70%' }}>70%</span></span>
+                    </li>
+                    <li>
+                        <h3>Data Structures & Algorithms</h3>
+                        <span className="bar"><span className="ds-algo" style={{ width: '50%' }}>50%</span></span>
+                    </li>
+                    <li>
+                        <h3>Linux</h3>
+                        <span className="bar"><span className="linux" style={{ width: '50%' }}>50%</span></span>
+                    </li>
+                    <li>
+                        <h3>Git</h3>
+                        <span className="bar"><span className="git" style={{ width: '75%' }}>75%</span></span>
+                    </li>
+                </Carousel>
             </div>
+            }
+            </TrackVisibility>
         </Container>
     )
 }
